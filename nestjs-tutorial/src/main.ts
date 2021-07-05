5ef5d381021690e6d211a338447c118b13cf204e
yuki <yuki.onishi1129@gmail.com>
2021-07-05 13:29:15 +0900

    add: main.ts comment

--- nestjs-tutorial/src/main.ts
+++ nestjs-tutorial/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // Nestアプリケーションのインスタンス生成
   const app = await NestFactory.create(AppModule);
+  // HTTPリスナー起動
   await app.listen(3000);
 }
 bootstrap();
