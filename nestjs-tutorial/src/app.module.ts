5f8fd88e6880bc323823e865288f7732cfb47f1f
yuki <yuki.onishi1129@gmail.com>
2021-07-05 13:54:37 +0900

    add: redirect

--- nestjs-tutorial/src/app.module.ts
+++ nestjs-tutorial/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+/* controllers */
 import { AppController } from './app.controller';
+import { CatsController } from './cats.controller';
+/* services */
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  // controllerを定義
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ nestjs-tutorial/src/cats.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Req, Post, Redirect, Query } from '@nestjs/common';
+import { Request } from 'express';
+
+// @Controller(): コントローラーを定義するデコレータ
+@Controller('cats')
+export class CatsController {
+  // post送信
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(@Req() request: Request): string {
+    return 'This action returns all cats';
+  }
+
+  @Get('dog')
+  // リダイレクト
+  @Redirect('http://localhost:3000/', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+}
